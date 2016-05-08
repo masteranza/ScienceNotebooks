0375ab83ba512bd49cb9940e1bf8a3b91770fa4b
Michal Mandrysz <masteranza@gmail.com>
2016-05-08 14:10:35 +0200

    - Improved exporting to Anki for images inside LaTeX - Improved referencing equations from other notebooks - Moved AnkiExporter and PackageUtils to Autoload - Modified Kernel/init.m - Small improvements in Science stylesheet

---
+++ Mathematica/Autoload/PackageUtils/Kernel/init.m
@@ -0,0 +1,3 @@
+(* ::Package:: *)
+
+Get["PackageUtils`PackageUtils`"];
---
+++ Mathematica/Autoload/PackageUtils/PacletInfo.m
@@ -0,0 +1,14 @@
+(* ::Package:: *)
+
+Paclet[
+	Name -> "ScienceReport",
+	Version -> "0.5",
+	MathematicaVersion -> "8+",
+	Extensions -> {{
+		"Documentation", 
+		Resources -> {
+			"Guides/ScienceReport"
+		}, 
+		Language -> "English"
+	}}
+]
