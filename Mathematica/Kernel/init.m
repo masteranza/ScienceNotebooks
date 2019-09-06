b3f30c3e8a62364cac3766f49203bd9205f5f43b
Michal Mandrysz <masteranza@gmail.com>
2019-09-06 03:04:36 +0200

    Little additions to Kernel init.m

--- Mathematica/Kernel/init.m
+++ Mathematica/Kernel/init.m
@@ -13,3 +13,14 @@ PlusMinus[{x_, err_}] :=
      ExponentFunction -> (Null &)], ")", " \[Times] ", 
     DisplayForm@SuperscriptBox["10", ToString[xE - 1]]}]];
 PlusMinus[x_, err_] := PlusMinus[{x, err}];
+
+EvaluatedAt[expr_, Automatic, min_, max_] := EvaluatedAt[expr, Replace[Reduce`FreeVariables[expr], {{v_,___}->v, _->None}], min, max];
+EvaluatedAt[expr_, x_, min_, max_] := (expr /. x->max) - (expr /. x->min);
+DiffOp[expr_] := Replace[Reduce`FreeVariables[expr],
+    {
+    {} -> expr,
+    {x_} :> D[expr, x],
+    x_List :> D[expr, {x}]
+    }
+];
+System`Convert`TeXFormDump`$TeXDelimiterReplacements = System`Convert`TeXFormDump`$TeXDelimiterReplacements /. {"\\left| " | "\\right| " -> "|","\\left\\| " | "\\right\\| " -> "\\| "};
